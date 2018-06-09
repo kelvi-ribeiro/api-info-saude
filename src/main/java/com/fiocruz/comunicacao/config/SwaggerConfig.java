4ed21bdcb09f6b2dab372fc305d55930ff756fa6
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-06-09 03:31:28 -0300

    Corrigindo caminho dos resources

--- src/main/java/com/fiocruz/comunicacao/config/SwaggerConfig.java
+++ src/main/java/com/fiocruz/comunicacao/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.macuxi.camarao.resources"))
+                .apis(RequestHandlerSelectors.basePackage("com.fiocruz.comunicacao.resources"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(metaData());
