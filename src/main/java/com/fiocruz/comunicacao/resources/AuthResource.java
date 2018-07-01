9729e9a2dc72db21b129d6633e66d701e6807bc2
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-07-01 01:42:55 -0300

    Comentando endpoint com problema

--- src/main/java/com/fiocruz/comunicacao/resources/AuthResource.java
+++ src/main/java/com/fiocruz/comunicacao/resources/AuthResource.java
@@ -23,14 +23,14 @@ public class AuthResource {
 	@Autowired
 	private AuthService service;
 	
-	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
-	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
-		UserSS user = UserService.authenticated();
-		String token = jwtUtil.generateToken(user.getUsername()); 
-		response.addHeader("Authorization", "Bearer " + token);
-		response.addHeader("access-control-expose-headers", "Authorization");
-		return ResponseEntity.noContent().build();
-	}
+//	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+//	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+//		UserSS user = UserService.authenticated();
+//		String token = jwtUtil.generateToken(user.getUsername()); 
+//		response.addHeader("Authorization", "Bearer " + token);
+//		response.addHeader("access-control-expose-headers", "Authorization");
+//		return ResponseEntity.noContent().build();
+//	}
 
 //	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
 //	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
