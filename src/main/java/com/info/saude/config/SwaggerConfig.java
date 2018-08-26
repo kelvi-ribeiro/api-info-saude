aa64a9d24681c316022655badc00a6d1be7f3e93
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-08-26 00:15:59 -0300

    Corrigindo problema do path do swagger dando status http 403

--- src/main/java/com/info/saude/config/SwaggerConfig.java
+++ src/main/java/com/info/saude/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.fiocruz.comunicacao.resources"))
+                .apis(RequestHandlerSelectors.basePackage("com.info.saude.resources"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(metaData());
