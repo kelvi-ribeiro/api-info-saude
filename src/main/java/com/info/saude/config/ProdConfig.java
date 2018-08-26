ad32036385fe029815ee4712690d6530a91c93c5
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-08-26 00:53:22 -0300

    Adicionando bean de email service na config de prod

--- src/main/java/com/info/saude/config/ProdConfig.java
+++ src/main/java/com/info/saude/config/ProdConfig.java
@@ -1,23 +1,18 @@
 package com.info.saude.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.info.saude.services.email.EmailService;
+import com.info.saude.services.email.SmtpEmailService;
 
 @Configuration
 @Profile("prod")
 public class ProdConfig {
 
-//	@Autowired
-//	private DBService dbService;
-//
-//	@Bean
-//	public boolean instantiateDatabase() throws Exception {
-//		dbService.instantiateTestDatabase();
-//		return true;
-//	}
-//	
-//	@Bean
-//	public EmailService emailService() {
-//		return new SmtpEmailService();
-//	}
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
 }
