551c2f15b4dfe9c5ee01048ed9a548245239d620
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-08-27 04:30:24 -0300

    Criando entidade e repository de TipoMedicamento

---
+++ src/main/java/com/info/saude/domain/TipoMedicamento.java
@@ -0,0 +1,63 @@
+package com.info.saude.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tipo_medicamento")
+public class TipoMedicamento implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String nome;
+
+	@Column(name = "caminho_imagem")
+	private String caminhoImagem;
+
+	public TipoMedicamento() {
+		super();
+	}
+
+	public TipoMedicamento(Integer id, String nome, String caminhoImagem) {
+		this.id = id;
+		this.nome = nome;
+		this.caminhoImagem = caminhoImagem;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCaminhoImagem() {
+		return caminhoImagem;
+	}
+
+	public void setCaminhoImagem(String caminhoImagem) {
+		this.caminhoImagem = caminhoImagem;
+	}
+
+}
