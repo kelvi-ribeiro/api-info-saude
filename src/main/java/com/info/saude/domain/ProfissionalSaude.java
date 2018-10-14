327f0c9101537636e4462378ce5434fdf1666454
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-10-14 20:35:45 -0300

    Criando entidade de ProfissionalSaude

---
+++ src/main/java/com/info/saude/domain/ProfissionalSaude.java
@@ -0,0 +1,52 @@
+package com.info.saude.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class ProfissionalSaude implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@ManyToOne()
+	@JoinColumn(name = "pessoa_id")
+	private Pessoa pessoa;
+
+	public ProfissionalSaude() {
+		super();
+	}
+
+	public ProfissionalSaude(Integer id, Pessoa pessoa) {
+		super();
+		this.id = id;
+		this.pessoa = pessoa;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	
+	
+
+}
