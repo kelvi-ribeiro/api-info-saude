c6213b536ad21a65b4b8e19451acbfcd2f41c690
root <root@kelvi-K46CB>
2018-10-15 02:54:17 -0300

    Criacao da entidade de linha de cuidado e tipo sanguineo

---
+++ src/main/java/com/info/saude/domain/LinhaCuidado.java
@@ -0,0 +1,59 @@
+package com.info.saude.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "linha_cuidado")
+public class LinhaCuidado implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String nome;
+
+	private String descricao;
+
+	public LinhaCuidado() {
+		super();
+	}
+
+	public LinhaCuidado(Integer id, String nome, String descricao) {
+
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
