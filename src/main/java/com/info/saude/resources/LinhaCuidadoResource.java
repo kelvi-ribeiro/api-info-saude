c4d9b5a5ad4682ecd1bf45e39480f906dbf178e2
root <root@kelvi-K46CB>
2018-10-15 03:03:09 -0300

    Criando listagem das linhas de cuidado e pequeno ajuste na nomenclatura do metodo de listagem de cidades

--- src/main/java/com/info/saude/resources/LinhaCuidadoResource.java
+++ src/main/java/com/info/saude/resources/LinhaCuidadoResource.java
@@ -8,19 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.info.saude.domain.Cidade;
-import com.info.saude.services.CidadeService;
+import com.info.saude.domain.LinhaCuidado;
+import com.info.saude.services.LinhaCuidadoService;
 
 @RestController
-@RequestMapping(value = "/cidades")
-public class CidadeResource {
+@RequestMapping(value = "/linhas-cuidado")
+public class LinhaCuidadoResource {
 
 	@Autowired
-	private CidadeService service;
+	private LinhaCuidadoService service;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<Cidade>> findAll() {
-		List<Cidade> list = service.findAll();
+	public ResponseEntity<List<LinhaCuidado>> findAllAtivos() {
+		List<LinhaCuidado> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
