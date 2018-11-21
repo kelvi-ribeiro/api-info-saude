b3b501f1b4b4f0378675c4494df65aee2e8d2fd3
kelvi.ribeiro <kelvi.ribeiro@DQX-DTI-81.unigranrio.br>
2018-11-21 18:12:14 -0200

    Setando time zone BR no arquivo main do SpringBootApplication

--- src/main/java/com/info/saude/ApiInfoSaudeApplication.java
+++ src/main/java/com/info/saude/ApiInfoSaudeApplication.java
@@ -1,5 +1,9 @@
 package com.info.saude;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +16,11 @@ public class ApiInfoSaudeApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiInfoSaudeApplication.class, args);
 	}
+	
+	@PostConstruct
+    void started() {
+      TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 
 	@Override
 	public void run(String... args) throws Exception {
